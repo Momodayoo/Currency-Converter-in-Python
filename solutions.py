51827625e59c1a7ad90346ceca6f8e4071368871
momodayoo <msyauqi94@gmail.com>
2024-04-07 15:48:07 +0800

    api linked and tested

---
+++ solutions.py
@@ -0,0 +1,12 @@
+import requests
+
+from_currency = str (
+    input("Enter the currency code: ")).upper()
+
+to_currency = str (
+    input("Enter the currency code you want to convert to: ")).upper()
+
+amount = float(input("Enter the amount you want to convert: "))
+
+response = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+print (response.status_code)
